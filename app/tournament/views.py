89ee54a2b349560405a9de2e7387ef79b173e440
João Godoi <joaogodoi.dev@gmail.com>
2026-05-21 01:09:31 -0300

    feat: add tournament app with views, urls and placeholder templates

---
+++ app/tournament/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def groups(request):
+    return render(request, 'tournament/groups.html')
+
+
+def playoffs(request):
+    return render(request, 'tournament/playoffs.html')
+
+
+def placements(request):
+    return render(request, 'tournament/placements.html')
