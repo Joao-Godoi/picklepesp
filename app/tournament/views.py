d79be7f3d293eaddf526ee26a323524e03240f90
João Godoi <joaogodoi.dev@gmail.com>
2026-05-21 02:34:46 -0300

    feat: wire views with selectors to serve dynamic group and bracket data

--- app/tournament/views.py
+++ app/tournament/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
+from tournament.selectors import get_groups_page_data, get_playoffs_page_data, get_placements_page_data
+
 
 def groups(request):
-    return render(request, "tournament/groups.html")
+    return render(request, "tournament/groups.html", {"groups_data": get_groups_page_data()})
 
 
 def playoffs(request):
-    return render(request, "tournament/playoffs.html")
+    return render(request, "tournament/playoffs.html", get_playoffs_page_data())
 
 
 def placements(request):
-    return render(request, "tournament/placements.html")
+    return render(request, "tournament/placements.html", get_placements_page_data())
